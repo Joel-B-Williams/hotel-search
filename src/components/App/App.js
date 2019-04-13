189b3df78cb5111a29a0905c07a2cfb3e0b33695
Joel Williams <joel.b.williams.is@gmail.com>
2019-04-12 22:12:22 -0500

    display Hotel components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,60 +2,22 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './style.less';
 
+import Hotel from './Hotel';
+
 class App extends Component {
     state = {
-        mockData: {
-            id: '907',
-            rewards: {
-                miles: 10000
-            },
-            lowestAveragePrice: {
-                currency: 'USD',
-                symbol: '&#36;',
-                amount: 579
-            },
-            hotelStaticContent: {
-                hotelId: 907,
-                languageCode: 'en',
-                mainImage: {
-                    category: 'EXTERIOR',
-                    url: 'http://d2whcypojkzby.cloudfront.net/imageRepo/2/0/68/56/314/ExteriorCarsGone_S.jpg',
-                    source: 'VFML'
-                },
-                name: 'Omni Chicago Hotel & Suites Magnificent Mile',
-                neighborhoodName: 'Magnificent Mile',
-                address: {
-                    line1: '676 North Michigan Avenue',
-                    line2: null,
-                    city: 'Chicago',
-                    stateName: 'Illinois',
-                    stateCode: 'IL',
-                    countryName: 'United States',
-                    countryCode: 'US',
-                    zipCode: '60611',
-                    latitude: 41.89475,
-                    longitude: -87.62465,
-                    timeZoneId: 'America/Chicago'
-                },
-                stars: 4,
-                rating: 9,
-                numberOfReviews: 685,
-                brandCode: '1324',
-                brandName: 'Omni Hotels',
-                propertyType: 204,
-                propertyTypeName: 'Hotel'
-            }
-        }
+        apiData: {}
     }
 
     componentDidMount() {
         axios
         .get('https://homework-app.rocketmiles.com/fe-homework/rates')
-        .then(res => console.log(res.data))
+        .then(res => this.setState({apiData: res.data}))
         .catch(err => console.log(err));
     }
 
     render() {
+        this.state.apiData.results ? console.log(this.state.apiData.results.hotels) : null
         return (
             <div className="app-container">
                 <div className="content">
@@ -65,22 +27,9 @@ class App extends Component {
                         Price
                         <button>sort</button>
                     </div>
-                    <div className="hotel-list">
-                    <img src={this.state.mockData.hotelStaticContent.mainImage.url} className="photo"/>
-                    <div className="details">
-                        <div>
-                            {this.state.mockData.hotelStaticContent.name}
-                        </div>
-                        <div>
-                            {this.state.mockData.hotelStaticContent.neighborhoodName}
-                        </div>
-                    </div>
-                    <div className="price">
-                        {this.state.mockData.lowestAveragePrice.symbol}
-                        {this.state.mockData.lowestAveragePrice.amount}
-                        {this.state.mockData.rewards.miles}
-                        <button>Select</button>
-                    </div>
+                    <div>
+                        { this.state.apiData.results ?
+                        this.state.apiData.results.hotels.map((hotel) => (<Hotel key={hotel.id} {...hotel} />)) : null }
                     </div>
                 </div>
             </div>
