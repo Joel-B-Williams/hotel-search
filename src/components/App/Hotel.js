d35f8a0b28784aa3ecae34978688e3687d16da16
Joel Williams <joel.b.williams.is@gmail.com>
2019-04-15 22:00:45 -0500

    basic error handling

--- src/components/App/Hotel.js
+++ src/components/App/Hotel.js
@@ -2,23 +2,41 @@ import React, { Component } from 'react';
 
 class Hotel extends Component {
 
-  render () 
-  {
+  defaultImage = (ev) => {
+    ev.target.src = "https://i.imgur.com/qUvaEAl.png"
+  }
+
+  render () {
     return (
       <div className="hotel-list">
-        <img src={this.props.hotelStaticContent.mainImage.url} className="photo"/>
+        <img src={this.props.hotelStaticContent.mainImage.url} onError={this.defaultImage} className="photo"/>
         <div className="details">
           <div>
-            {this.props.hotelStaticContent.name}
+            {this.props.hotelStaticContent.name || "No name found" }
           </div>
           <div>
-            {this.props.hotelStaticContent.neighborhoodName}
+            {this.props.hotelStaticContent.neighborhoodName || "Location unknown"}
           </div>
         </div>
         <div className="price">
-          {this.props.lowestAveragePrice.symbol}
-          {this.props.lowestAveragePrice.amount}
-          {this.props.rewards.miles}
+          <table>
+            <tbody>
+              <tr>
+                <td>Price</td>
+                <td>
+                  {/* find less risky method if possible */}
+                  <p dangerouslySetInnerHTML={{__html: this.props.lowestAveragePrice.symbol}}></p>
+                  {this.props.lowestAveragePrice.amount || "$$"}
+                </td>
+              </tr>
+              <tr>
+                <td>Miles</td>
+                <td>
+                  {this.props.rewards.miles || "Reward unavailable"}
+                </td>
+              </tr>
+            </tbody>
+          </table>
           <button>Select</button>
         </div>
       </div>
